8b9fb606285e4c77aa410cbeb30aef8916c43af5
Polevoy Dmitry <dvpsun@users.noreply.github.com>
2024-03-25 19:39:46 +0300

    fix: operator+= stupid dummy realization

--- prj.lab/complex/complex.cpp
+++ prj.lab/complex/complex.cpp
@@ -6,11 +6,23 @@ Complex Complex::operator-() const noexcept { return Complex(-re, -im); }
 bool Complex::operator==(const Complex& rhs) const noexcept { return true; }
 bool Complex::operator!=(const Complex& rhs) const noexcept { return true; }
 
-Complex& Complex::operator+=(const Complex& rhs) noexcept { return *this; }
+Complex& Complex::operator+=(const Complex& rhs) noexcept { 
+  re += rhs.re;
+  im += rhs.im;
+  return *this;
+}
+
 Complex& Complex::operator+=(const double rhs) noexcept { return operator+=(Complex(rhs)); }
 
-Complex& Complex::operator-=(const Complex& rhs) noexcept { return *this; }
-Complex& Complex::operator-=(const double rhs) noexcept { return operator-=(Complex(rhs)); }
+Complex& Complex::operator-=(const Complex& rhs) noexcept { 
+  re -= rhs.re;
+  im -= rhs.im;
+  return *this;
+}
+
+Complex& Complex::operator-=(const double rhs) noexcept { 
+  return operator-=(Complex(rhs)); 
+}
 
 Complex& Complex::operator*=(const Complex& rhs) noexcept {  return *this; }
 Complex& Complex::operator*=(const double rhs) noexcept { return *this; }
@@ -22,11 +34,11 @@ std::ostream& Complex::WriteTo(std::ostream& ostrm) const noexcept { return ostr
 std::istream& Complex::ReadFrom(std::istream& istrm) noexcept { return istrm; }
 
 
-Complex operator+(const Complex& lhs, const Complex& rhs) noexcept { return Complex(lhs) += rhs; }
+Complex operator+(Complex lhs, const Complex& rhs) noexcept { return lhs += rhs; }
 Complex operator+(const Complex& lhs, const double rhs) noexcept { return Complex(lhs) += rhs; }
 Complex operator+(const double lhs, const Complex& rhs) noexcept { return Complex(lhs) += rhs; }
 
-Complex operator-(const Complex& lhs, const Complex& rhs) noexcept { return Complex(lhs) -= rhs; }
+Complex operator-(Complex lhs, const Complex& rhs) noexcept { return lhs -= rhs; }
 Complex operator-(const Complex& lhs, const double rhs) noexcept { return Complex(lhs) -= rhs; }
 Complex operator-(const double lhs, const Complex& rhs) noexcept { return Complex(lhs) -= rhs; }
 
